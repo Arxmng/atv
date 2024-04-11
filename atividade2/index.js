8fc0021858100cadc1417ac0b179ccf857101d7d
Vitor Hugo <vitorhugo7b@gmail.com>
2024-04-11 20:00:27 -0300

    Atividade 5 concluída :9

--- atividade2/index.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')()
-
-
-const salario = Number(prompt('Digite o Salario'))
-const percentual = Number(prompt('Digite o percentual de reajuste'))
-
-const  salarionovo = salario + (salario * (percentual/100))
-
-console.log(salarionovo);
